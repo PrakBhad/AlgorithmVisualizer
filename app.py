d4d05b201ddb9dfa69041d499a21dd5d93e681b8
PrakBhad <prakharbhadauria2004@gmail.com>
2025-01-30 11:35:33 +0530

    Added Boundary,Height Fix

--- app.py
+++ app.py
@@ -6,43 +6,58 @@ import pygame
 from pygame.locals import *
 import random
 
+
 class Rect(pygame.sprite.Sprite):
-    def __init__(self, x=100, y=0, width=100, height=500, color=(0, 255, 0), value=10):
+    def __init__(self, height, x=0, y=0, width=10, color=(0, 255, 0), value=10):
         super().__init__()
         self.image = pygame.Surface([width, height])
         self.image.fill(color)
         self.value = value
         # Fetch the rectangle object that has the dimensions of the image
         # Update the position of this object by setting the values of rect.x and rect.y
-        self.rect = self.image.get_rect(center=(x, y))
+        self.rect = self.image.get_rect(midbottom=(x, y))
+
+
 def main():
     pygame.init()
     screen = pygame.display.set_mode(
         (1280, 720), flags=DOUBLEBUF)
     clock = pygame.time.Clock()
     running = True
-    
+
     rects = pygame.sprite.Group()
-    x=50
-    height = 500
-    arr= [random.randint(-200,200) for i in range(100)]
-    
+    height = 0
+    x = 10
+    screen_width = screen.get_width()
+    screen_height = screen.get_height()
+    rect_width = 10  # Fixed width for rectangles
+    spacing = 5  # Space between rectangles
+
+    arr = [random.randint(-100, 100) for i in range(100)]
+
     for i in range(len(arr)):
-            rect = Rect(x=x,y=screen.get_height(),height=height) #Centered the rectangles
-            rects.add(rect)
-            x+=rect.image.get_width()+20
-            height=max(100,rect.image.get_height()+arr[i])
-    
+        # Centered the rectangles
+        if x + rect_width > screen_width:
+            break
+
+        height = max(abs(arr[i]), height+arr[i])
+        if height >= screen_height:
+            height = abs(arr[i])
+            
+        rect = Rect(x=x, y=screen_height, height=height)
+        rects.add(rect)
+        x += rect.image.get_width()+spacing
+
     while running:
         # poll for events
         # pygame.QUIT event means the user clicked X to close your window
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-        
+
         # fill the screen with a color to wipe away anything from last frame
         screen.fill("black")
-        
+
         rects.draw(screen)
         # flip() the display to put your work on screen
         pygame.display.flip()
