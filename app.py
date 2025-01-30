6675b5aea493624192eedaad3e7b918259ae6841
PrakBhad <prakharbhadauria2004@gmail.com>
2025-01-30 15:25:31 +0530

    Added rectangle Generation into Rect Class

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import random
 
 
 class Rect(pygame.sprite.Sprite):
-    def __init__(self, height, x=0, y=0, width=10, color=(0, 255, 0), value=10):
+    def __init__(self, height=0, x=0, y=0, width=10, color=(0, 255, 0), value=10):
         super().__init__()
         self.image = pygame.Surface([width, height])
         self.image.fill(color)
@@ -16,7 +16,26 @@ class Rect(pygame.sprite.Sprite):
         # Fetch the rectangle object that has the dimensions of the image
         # Update the position of this object by setting the values of rect.x and rect.y
         self.rect = self.image.get_rect(midbottom=(x, y))
+    
+    def randomize(self,rect:Rect,rects:pygame.sprite.Group,arr:list):
+        screen_width,screen_height = pygame.display.get_window_size()
+        rect_width = 10  # Fixed width for rectangles
+        spacing = 5  # Space between rectangles 
+        height = 0
+        x = 10
+        print(screen_width)
+        print(screen_height) 
+        for i in range(len(arr)):
+            if x + rect_width > screen_width:  # Width Check
+                break
+            
+            height = arr[i]  # Height Check
 
+            rect = Rect(x=x, y=screen_height, height=height)
+            rects.add(rect)
+            x += rect.image.get_width()+spacing #Spacing
+            
+        return rects
 
 def main():
     pygame.init()
@@ -24,18 +43,15 @@ def main():
         (1280, 720), flags=DOUBLEBUF)
     clock = pygame.time.Clock()
     running = True
-
-    rects = pygame.sprite.Group()
-    height = 0
-    x = 10
-    screen_width = screen.get_width()
-    screen_height = screen.get_height()
-    rect_width = 10  # Fixed width for rectangles
-    spacing = 5  # Space between rectangles
-
-    arr = [random.randint(0, 500) for i in range(100)]
-    srt_arr = arr
     
+    rect = Rect()
+    rects = pygame.sprite.Group()
+    arr = [random.randint(0, 500) for i in range(100)]
+    rects = rect.randomize(rect,rects,arr)
+    
+    #arr = [random.randint(0, 500) for i in range(100)]
+    #srt_arr = arr
+    """
     for i in range(len(srt_arr)):
         if x + rect_width > screen_width:  # Width Check
             break
@@ -46,6 +62,7 @@ def main():
         rects.add(rect)
         
         x += rect.image.get_width()+spacing #Spacing
+    """
 
     while running:
         # poll for events
