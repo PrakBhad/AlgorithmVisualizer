780ae4bf038875838fa78087bc974d0dec04c4a2
PrakBhad <prakharbhadauria2004@gmail.com>
2025-01-30 15:53:17 +0530

    Added Proper randomize with key press inside visualizer

--- app.py
+++ app.py
@@ -17,14 +17,15 @@ class Rect(pygame.sprite.Sprite):
         # Update the position of this object by setting the values of rect.x and rect.y
         self.rect = self.image.get_rect(midbottom=(x, y))
     
-    def randomize(self,rect:Rect,rects:pygame.sprite.Group,arr:list):
+    @staticmethod
+    def randomize(rects:pygame.sprite.Group):
         screen_width,screen_height = pygame.display.get_window_size()
         rect_width = 10  # Fixed width for rectangles
         spacing = 5  # Space between rectangles 
         height = 0
         x = 10
-        print(screen_width)
-        print(screen_height) 
+        rects.empty()
+        arr = [random.randint(0, 500) for i in range(100)]
         for i in range(len(arr)):
             if x + rect_width > screen_width:  # Width Check
                 break
@@ -34,7 +35,7 @@ class Rect(pygame.sprite.Sprite):
             rect = Rect(x=x, y=screen_height, height=height)
             rects.add(rect)
             x += rect.image.get_width()+spacing #Spacing
-            
+        arr = []
         return rects
 
 def main():
@@ -46,37 +47,22 @@ def main():
     
     rect = Rect()
     rects = pygame.sprite.Group()
-    arr = [random.randint(0, 500) for i in range(100)]
-    rects = rect.randomize(rect,rects,arr)
+    rects = rect.randomize(rects)
     
-    #arr = [random.randint(0, 500) for i in range(100)]
-    #srt_arr = arr
-    """
-    for i in range(len(srt_arr)):
-        if x + rect_width > screen_width:  # Width Check
-            break
-
-        height = srt_arr[i]  # Height Check
-
-        rect = Rect(x=x, y=screen_height, height=height)
-        rects.add(rect)
-        
-        x += rect.image.get_width()+spacing #Spacing
-    """
-
     while running:
         # poll for events
         # pygame.QUIT event means the user clicked X to close your window
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                running = False
+                running = False             
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_r: #Press r to randomize
+                    rects = rect.randomize(rects)
 
-        # fill the screen with a color to wipe away anything from last frame
-        screen.fill("black")
-
-        rects.draw(screen)
-        # flip() the display to put your work on screen
-        pygame.display.flip()
+        screen.fill("black")  # fill the screen with a color to wipe away anything from last frame
+        rects.draw(screen) # Draw rectangles onto screen
+        
+        pygame.display.flip() # flip() the display to put your work on screen
         clock.tick(60)  # limits FPS to 60
 
     pygame.quit()
