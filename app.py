1652ca34d97006831ca151cb27a85d657eee9f58
PrakBhad <prakharbhadauria2004@gmail.com>
2025-02-02 01:49:10 +0530

    Graphing Mode Check and remove Displayer Class, turned fps toggler into a simple function, user can call

--- app.py
+++ app.py
@@ -41,13 +41,12 @@ class Rect(pygame.sprite.Sprite):
         return rects
 
 
-class Displayer:
-    @staticmethod
-    def fps_toggler(cmpmode: bool):
-        if cmpmode:
-            return 5
-        else:
-            return 600
+
+def fps_toggler(cmpmode: bool):
+    if cmpmode:
+        return 5
+    else:
+        return 600
 
 
 def main():
@@ -67,9 +66,11 @@ def main():
     sorting_selection = False
     sorting_merge = False
     sort_ascending = True  # True for ascending, False for descending
+    
+    graphing_mode = False
 
     cmpmode = False
-    fps = Displayer.fps_toggler(cmpmode)
+    fps = fps_toggler(cmpmode)
 
     paused = False
 
@@ -148,7 +149,7 @@ def main():
                 elif event.key == pygame.K_c:
                     if not (sorting_bubble or sorting_insertion or sorting_selection or sorting_merge):
                         cmpmode = not cmpmode
-                        fps = Displayer.fps_toggler(cmpmode)
+                        fps = fps_toggler(cmpmode)
 
                 elif event.key == pygame.K_SPACE:  # Press SPACE to pause/unpause
                     paused = not paused
@@ -162,9 +163,11 @@ def main():
                     sorting_bubble = sorting_insertion = sorting_merge = sorting_selection = False
                     sort_generator = None
 
-        # Fill the screen with a color to wipe away anything from last frame
-        screen.fill("black")
-        rects.draw(screen)  # Draw rectangles onto screen
+        if not graphing_mode:    
+            screen.fill("black")
+            rects.draw(screen)  # Draw rectangles onto screen
+        else:
+            print()
 
         pygame.display.flip()  # Flip the display to put your work on screen
         clock.tick(fps)
