91b5d30ec5ce3a0aaa383ca75c3836dd0aeca33b
PrakBhad <prakharbhadauria2004@gmail.com>
2025-02-09 00:24:33 +0530

    Added Pseudo Maze Generation, Need to get position generation working

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from Sorting_Algorithms.bubblesort import *
 from Sorting_Algorithms.insertionsort import *
 from Sorting_Algorithms.selectionsort import *
 from Sorting_Algorithms.mergesort import *
-
 import pygame
 from pygame.locals import *
 import random
@@ -22,21 +21,67 @@ class Rect(pygame.sprite.Sprite):
         screen_width, screen_height = pygame.display.get_window_size()
         rect_width = 10  # Fixed width for rectangles
         spacing = 5  # Space between rectangles
-        height = 0
-        x = 15
+        rect_height = 0
+        x = 10
         rects.empty()
-        count = 0
         arr = [random.randint(120, 500) for i in range(screen_width)]
         for i in range(len(arr)):
             if x + rect_width > screen_width:  # Width Check
                 break
-            count = i + 1
-            height = abs(arr[i])  # Height Check
+            rect_height = abs(arr[i])  # Height Check
 
-            rect = Rect(x=x, y=screen_height, height=height, width=rect_width)
+            rect = Rect(x=x, y=screen_height, height=rect_height, width=rect_width)
             rects.add(rect)
             x += rect.image.get_width() + spacing  # Spacing
-        print(count)
+
+        print(len(rects))
+        return rects
+
+    def maze(self,rects:pygame.sprite.Group):
+        screen_width, screen_height = pygame.display.get_window_size()
+        rects.empty()
+        y = screen_height // 2
+        count = 0
+        rect_height = 10
+        rect_width = 10
+
+        #Left End
+        for x in range(0,screen_width+rect_width,10):
+            rect = Rect(x=x, y=y, height=rect_height, width=rect_width, color=(255,255,255))
+            rects.add(rect)
+
+        #Right End
+        for x in range(0,screen_width+rect_width,10):
+            rect = Rect(x=x, y=screen_height, height=rect_height, width=rect_width, color=(255,255,255))
+            rects.add(rect)
+
+        #Left Vertical Line
+        for y1 in range(y,screen_height,10):
+            rect = Rect(x=rect_width//2, y=y1, height=rect_height, width=rect_width, color=(255,255,255))
+            rects.add(rect)
+
+        #Right Vertical Line
+        for y1 in range(y,screen_height+rect_height,10):
+            rect = Rect(x=(screen_width-rect_width//2), y=y1, height=rect_height, width=rect_width, color=(255,255,255))
+            rects.add(rect)
+
+        isAlternate = 0
+        for x in range(30, screen_width, 20):  # Iterate over columns with spacing of 20
+            if isAlternate % 2 == 0:  # "Up" line
+                for y1 in range(y, y + y // 2, 10):  # Create rectangles going upwards
+                    rect = Rect(x=x, y=y1, height=rect_height, width=rect_width, color=(255, 255, 255))
+                    if not pygame.sprite.spritecollideany(rect, rects):  # Check for intersection
+                        rects.add(rect)  # Add only if no collision
+            else:  # "Down" line
+                for y1 in range(screen_height - (y // 2), screen_height + rect_height, 10):  
+                    # Create rectangles going downwards
+                    rect = Rect(x=x, y=y1, height=rect_height, width=rect_width, color=(255, 255, 255))
+                    if not pygame.sprite.spritecollideany(rect, rects):  # Check for intersection
+                        rects.add(rect)  # Add only if no collision
+            isAlternate += 1
+
+
+        print(len(rects))
         return rects
 
 
@@ -79,8 +124,17 @@ def main():
             if event.type == pygame.QUIT:
                 running = False
 
-            elif event.type == VIDEORESIZE:
+            elif event.type == VIDEORESIZE and not graphing_mode:
                 rects = rect.randomize(rects)
+                sort_generator = None
+                sorting_bubble = False
+                sorting_insertion = False
+                sorting_selection = False
+                sorting_merge = False
+                paused = False
+
+            elif event.type == VIDEORESIZE and graphing_mode:
+                rects = rect.maze(rects)
                 sort_generator = None
                 sorting_bubble = False
                 sorting_insertion = False
@@ -91,6 +145,15 @@ def main():
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_r and not graphing_mode:  # Press 'r' to randomize
                     rects = rect.randomize(rects)
+                    sort_generator = None
+                    sorting_bubble = False
+                    sorting_insertion = False
+                    sorting_selection = False
+                    sorting_merge = False
+                    paused = False
+
+                elif event.key == pygame.K_r and graphing_mode:  # Press 'r' to randomize
+                    rects = rect.maze(rects)
                     sort_generator = None
                     sorting_bubble = False
                     sorting_insertion = False
@@ -159,6 +222,8 @@ def main():
                     graphing_mode = not graphing_mode
                     if not graphing_mode:
                         rects = rect.randomize(rects)
+                    else:
+                        rects = rect.maze(rects)
 
                 elif event.key == pygame.K_c:
                     if not (sorting_bubble or sorting_insertion or sorting_selection or sorting_merge):
@@ -168,7 +233,7 @@ def main():
                 elif event.key == pygame.K_SPACE:  # Press SPACE to pause/unpause
                     paused = not paused
 
-                elif event.key == pygame.K_f:
+                elif event.key == pygame.K_f and not graphing_mode:
                     fullscreen = not fullscreen
                     if fullscreen:
                         screen = pygame.display.set_mode(
@@ -179,6 +244,16 @@ def main():
                             (1520, 860), flags=DOUBLEBUF | RESIZABLE)
                         rects = rect.randomize(rects)
 
+                elif event.key == pygame.K_f and graphing_mode:
+                    fullscreen = not fullscreen
+                    if fullscreen:
+                        screen = pygame.display.set_mode(
+                            (1520, 860), flags=DOUBLEBUF | FULLSCREEN)
+                        rects = rect.maze(rects)
+                    else:
+                        screen = pygame.display.set_mode(
+                            (1520, 860), flags=DOUBLEBUF | RESIZABLE)
+                        rects = rect.maze(rects)
         # Sorting logic (runs continuously when sorting is True and not paused)
         if not paused:
             if (sorting_bubble or sorting_insertion or sorting_selection or sorting_merge) and sort_generator is not None and not graphing_mode:
@@ -192,11 +267,9 @@ def main():
             screen.fill("black")
             rects.draw(screen)  # Draw rectangles onto screen
         else:
-            rects.empty()
             screen.fill("black")
             rects.draw(screen)
-
-        pygame.display.update()
+        
         pygame.display.flip()  # Flip the display to put your work on screen
         clock.tick(fps)
 
